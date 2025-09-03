0690eee8bd9db8cf5535fb4d6f48827841903e48
lrpalmer27 <lrpalmer@mun.ca>
2025-09-02 20:55:09 -0700

    moving files around to correctly render HTML overlay with the CSS styling

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/user/<username>")
+def profile(username):
+    return f'Hello, {username}'
+
+@app.route("/test")
+def renderHTML():
+    return render_template('overlay.html')
